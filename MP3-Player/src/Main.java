e3621951e57679987088209a1a5562e6bd663cfc
Niels <niels_r91@hotmail.com>
2014-05-02 15:24:46 +0200

    Extended Test File

--- MP3-Player/src/Main.java
+++ MP3-Player/src/Main.java
@@ -7,7 +7,48 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("42ES04C MP3-PLAYER");
-		System.out.println("FAget yourself manhwore");
+		System.out.println("WORLDS FINEST AND MOST ADVANCED PIECE OF JUNK");
+		System.out.println("TEST");
+		sleep(1000);
+		System.out.println("1");
+		sleep(1000);
+		System.out.println("2");
+		sleep(1000);
+		System.out.println("3");
+		sleep(1000);
+		System.out.println("4");
+		sleep(1000);
+		System.out.println("5");
+		sleep(1000);
+		System.out.println("6");
+		sleep(1000);
+		System.out.println("7");
+		sleep(1000);
+		System.out.println("8");
+		sleep(1000);
+		System.out.println("9");
+		sleep(1000);
+		System.out.println("10");
+		sleep(1000);
+		System.out.println("11");
+		sleep(1000);
+		System.out.println("12");
+		sleep(1000);
+		System.out.println("13");
+		sleep(1000);
+		System.out.println("15");
+		sleep(1000);
+		System.out.println("END OF TEST");
+	}
+	
+	public static void sleep(int a){
+		try{
+			Thread.sleep(a);
+		}catch(InterruptedException ex){
+			Thread.currentThread().interrupt();
+			
+		}
+		
 	}
 
 }
