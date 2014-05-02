e7d0f17f4beed326f3bd0fd84638df8807d96152
McMuffinton <spameverythinguwant@gmail.com>
2014-05-02 13:14:25 +0200

    main aangepast
    yolo

--- MP3-Player/src/Main.java
+++ MP3-Player/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("42ES04C MP3-PLAYER");
-
+		System.out.println("Niels is a faget");
 	}
 
 }
