31dcabafff3c8db2efd07474766330f800025b3a
McMuffinton <spameverythinguwant@gmail.com>
2014-05-16 13:46:58 +0200

    Declared instructions (READ/WRITE) and Declared register addresses for the VS1033

--- MP3-Player/src/Decoder.java
+++ MP3-Player/src/Decoder.java
@@ -1,4 +1,5 @@
 public class Decoder {
+	//Declare pins
     private int RST = 82;
     private int DREQ = 83;
     private int SCLK = 66;
@@ -7,6 +8,24 @@ public class Decoder {
     private int SI = 69;
     private int BSYNC = 101;
 
+    //Declare instructions
+    private byte WRITE = 0x02;
+    private byte READ = 0x03;
+    
+    //Declare addresses
+    private byte MODE = 0x00;
+    private byte STATUS = 0x01;
+    private byte BASSTREBLE = 0x02;
+    private byte CLOCKFREQ = 0x03;
+    private byte DECODE_TIME = 0x04;
+    private byte AUDATA = 0x05;
+    private byte WRAM = 0x06;
+    private byte WRAMADRESS = 0x07;
+    private byte VOLUME = 0x0B; //When using this: Most significant byte = left channel volume, least significant byte = right channel volume
+    //0xFEFE = silent
+    //0x0000 = LOUD
+    
+    
     public Decoder() {
 //        sendInstruction();
     }
